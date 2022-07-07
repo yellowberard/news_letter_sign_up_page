f8c315c52a3d098b96a0b49b8dfb1da27bc9c04b
Mayank Goel <goel.mayank235@gmail.com>
2022-07-08 02:14:31 +0530

    Added signu up page

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request")
+const request = require("request");
+const https = require("https");
 const app = express();
 
 app.use(bodyParser.urlencoded({
@@ -17,9 +18,36 @@ app.post("/", (req, res) => {
     const Fname = req.body.Fname;
     const Lname = req.body.Lname;
     const Email = req.body.Email;
-    res.send(Fname + Lname + Email);
+    var data = {
+        members: [{
+            email_address: Email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: Fname,
+                LNAME: Lname
+            }
+        }]
+    };
+    const jData = JSON.stringify(data);
+    const url = "https://us10.api.mailchimp.com/3.0/lists/8a6bd467df"
+    const options = {
+        method: "POST",
+        auth: "yellowbeard:d50d50818ab70537bdb1312e4ed18f91-us10"
+    }
+    const request = https.request(url, options, (response) => {
+        response.on("data", (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jData);
+    request.end();
 })
 
 app.listen(6969, () => {
     console.log("Server is running at Port:6969");
 });
+
+//api key
+//d50d50818ab70537bdb1312e4ed18f91-us10
+// unique mail chimp id/list id
+//8a6bd467df
